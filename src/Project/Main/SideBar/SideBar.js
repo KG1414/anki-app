3f2b51424b9ca93e2bd1065ef70ec898a24f9860
kylegallard <kylegallard@gmail.com>
2022-03-21 16:38:50 +1100

    Removed error. Added extra Link component without to= which was crashing this app

--- src/Project/Main/SideBar/SideBar.js
+++ src/Project/Main/SideBar/SideBar.js
@@ -2,21 +2,20 @@ import { Link } from "react-router-dom";
 import './SideBar.css';
 
 const SideBar = (props) => {
-    const openNav = () => {
-        //
-    }
+    // const openNav = () => {
+    //     //
+    // }
 
-    const closeNav = () => {
-        //
-    }
+    // const closeNav = () => {
+    //     //
+    // }
 
     return (
         <div id="mySidebar" className="sidebar">
             <h1>Anki App</h1>
-            <button className="closebtn" onclick={closeNav}>X</button>
+            <button className="closebtn">X</button>
             <Link className="nav__link" to="/home">Home</Link>
             <Link className="nav__link" to="/contact">Contact</Link>
-            <Link></Link>
         </div>
     );
 };
