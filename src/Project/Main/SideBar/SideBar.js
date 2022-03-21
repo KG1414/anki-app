37aeb7c8790b21bca6efc4c7541fa7a9a865c7d9
kylegallard <kylegallard@gmail.com>
2022-03-22 10:30:53 +1100

    Updated card css and styling throughout the App including font.

--- src/Project/Main/SideBar/SideBar.js
+++ src/Project/Main/SideBar/SideBar.js
@@ -2,20 +2,22 @@ import { Link } from "react-router-dom";
 import './SideBar.css';
 
 const SideBar = (props) => {
-    // const openNav = () => {
-    //     //
-    // }
+    const openNav = () => {
+        //
+    }
 
-    // const closeNav = () => {
-    //     //
-    // }
+    const closeNav = () => {
+        //
+    }
 
     return (
         <div id="mySidebar" className="sidebar">
             <h1>Anki App</h1>
             <button className="closebtn">X</button>
             <Link className="nav__link" to="/home">Home</Link>
+            <Link className="nav__link" to="/topic">Topic</Link>
             <Link className="nav__link" to="/contact">Contact</Link>
+
         </div>
     );
 };
